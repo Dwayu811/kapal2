1f895a351a699dc67e6a1ccc8087ea007d416190
Dwayu811 <cintyakasih445@gmail.com>
2025-12-18 19:09:58 +0800

    kapal dan bom

--- latihan.js
+++ latihan.js
@@ -1,13 +1,34 @@
-// Dapatkan elemen kotak berdasarkan ID
-const cursorBox = document.getElementById('cursor-box');
+const img = document.getElementById("cursor-img");
 
-// Tambahkan event listener untuk pergerakan mouse pada seluruh dokumen
-document.addEventListener('mousemove', (event) => {
-    // event.clientX adalah koordinat X horizontal kursor
-    // event.clientY adalah koordinat Y vertikal kursor
-
-    // Perbarui posisi kotak menggunakan CSS 'left' dan 'top'
-    // menggunakan style.left dan style.top untuk pembaruan posisi langsung
-    cursorBox.style.left = event.clientX + 'px';
-    cursorBox.style.top = event.clientY + 'px';
+document.addEventListener("mousemove", (e) => {
+    img.style.left = e.pageX + "px";
+    img.style.top = e.pageY + "px";
 });
+
+// Klik = lempar bom
+const gameArea = document.getElementById("game-area")
+
+document.addEventListener("click", (e) => {
+  createBomb(e.pageX, e.pageY);
+});
+
+function createBomb(x, y) {
+  const bomb = document.createElement("div");
+  bomb.className = "bomb";
+  bomb.style.left = x + "px";
+  bomb.style.top  = y + "px";
+  gameArea.appendChild(bomb);
+
+ let speed = 10; // kecepatan peluru
+
+  const shoot = setInterval(() => {
+    y -= speed;
+    bomb.style.top = y + "px";
+
+    // Hapus kalau keluar layar
+    if (y > window.innerWidth) {
+      clearInterval(shoot);
+      bomb.remove();
+    }
+  }, 16);
+}
